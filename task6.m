74131311f52f846a2e1a6b7ad9c053501b89087f
Elena-Jiyoung <jvc6739@psu.edu>
2024-11-15 18:16:08 -0500

    new approach(no helper functions, just use exact demo codes) for task 6 and delete pts1 and pts2 mat files, F matrices updated

--- task6.m
+++ task6.m
@@ -1,46 +1,52 @@
-% Function call
-% Load the images
-im1 = imread("im1corrected.jpg");
-im2 = imread("im2corrected.jpg");
 
-figure, imshow(im1);
-title('Select points in Image 1, then press Enter');
-[x1, y1] = getpts;
-pts1 = [x1, y1];
+%test eight point algorithm
+% Test the Eight-point Algorithm with and without Hartley Preconditioning
 
-figure, imshow(im2);
-title('Select corresponding points in Image 2, then press Enter');
-[x2, y2] = getpts;
-pts2 = [x2, y2];
+im = imread('im1corrected.jpg');
+im2 = imread('im2corrected.jpg');
 
-% save these points to be used in task 7
-save('pts1.mat', 'pts1');
-save('pts2.mat', 'pts2');
+F_with_hartley = eightpoint_with_hartley(im, im2);
+F_without_hartley = eightpoint_without_hartley(im, im2);
+save("F_with_Hartley.mat", "F_with_hartley");
+save("F_without_Hartley.mat", "F_without_hartley");
 
-% Load previously saved points
-load('pts1.mat');
-load('pts2.mat');
+% Display results
+disp("Fundamental Matrix with Hartley Preconditioning:");
+disp(F_with_hartley);
 
-
-[F_normalized, points1, points2] = eightpoint_with_hartley(im1, im2, pts1, pts2, 1, 2);
-[F_without_hartley, points_1, points_2] = eightpoint_without_hartley(im1, im2, pts1, pts2, 3, 4);
-
-% save these F matrices to be used in task 7
-save('F_with_Hartley.mat', 'F_normalized');
-save('F_without_Hartley.mat', 'F_without_hartley');
+disp("Fundamental Matrix without Hartley Preconditioning:");
+disp(F_without_hartley);
 
 %----------Eightpoint Algorithm 1. With Hartley Preconditioning and 
 % 2. Without Hartley Preconditionding-------------------------
 % First, Compute F using eightpoint algorithm with Hartley
 % preconditioning(normalization) as shown in the demo code.
-function [F_normalized, pts1, pts2] = eightpoint_with_hartley(im1, im2, pts1, pts2, fig1, fig2)
-
+function [F_with_hartley] = eightpoint_with_hartley(im, im2)
+    
+    figure(1); imagesc(im); axis image; drawnow;
+    figure(2); imagesc(im2); axis image; drawnow;
+    
+    figure(1); [x1,y1] = getpts;
+    figure(1); imagesc(im); axis image; hold on
+    for i=1:length(x1)
+       h=plot(x1(i),y1(i),'*'); set(h,'Color','g','LineWidth',2);
+       text(x1(i),y1(i),sprintf('%d',i));
+    end
+    hold off
+    drawnow;
+    
+    figure(2); imagesc(im2); axis image; drawnow;
+     [x2,y2] = getpts;
+    figure(2); imagesc(im2); axis image; hold on
+    for i=1:length(x2)
+       h=plot(x2(i),y2(i),'*'); set(h,'Color','g','LineWidth',2);
+       text(x2(i),y2(i),sprintf('%d',i));
+    end
+    hold off
+    drawnow;
+    
     %do Hartley preconditioning
-    % Extract x and y coordinates
-    x1 = pts1(:, 1);
-    y1 = pts1(:, 2);
-    x2 = pts2(:, 1);
-    y2 = pts2(:, 2);
+    savx1 = x1; savy1 = y1; savx2 = x2; savy2 = y2;
     mux = mean(x1);
     muy = mean(y1);
     stdxy = (std(x1)+std(y1))/2;
@@ -53,98 +59,208 @@ function [F_normalized, pts1, pts2] = eightpoint_with_hartley(im1, im2, pts1, pt
     T2 = [1 0 -mux; 0 1 -muy; 0 0 stdxy]/stdxy;
     nx2 = (x2-mux)/stdxy;
     ny2 = (y2-muy)/stdxy;
+    
     A = [];
-    for i=1:length(nx1)
-       A(i,:) = [nx1(i)*nx2(i) nx1(i)*ny2(i) nx1(i) ny1(i)*nx2(i) ny1(i)*ny2(i) ny1(i) nx2(i) ny2(i) 1];
+    for i=1:length(nx1);
+        A(i,:) = [nx1(i)*nx2(i) nx1(i)*ny2(i) nx1(i) ny1(i)*nx2(i) ny1(i)*ny2(i) ny1(i) nx2(i) ny2(i) 1];
     end
     %get eigenvector associated with smallest eigenvalue of A' * A
     [u,d] = eigs(A' * A,1,'SM');
     F = reshape(u,3,3);
+    
     %make F rank 2
     oldF = F;
     [U,D,V] = svd(F);
     D(3,3) = 0;
     F = U * D * V';
-    F_normalized = T2' * F * T1;
-    disp("Fundamental matrix F from Hartley preconditioning is: ");
-    disp(F_normalized);
     
-    % Draw epipolar lines for sanity check
-    sanity_check(im1, im2, F_normalized, pts1, pts2, fig1, fig2);
+    %unnormalize F to undo the effects of Hartley preconditioning
+    F = T2' * F * T1;
+    
+    colors =  'bgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmyk';
+    %overlay epipolar lines on im2
+    L = F * [x1' ; y1'; ones(size(x1'))];
+    [nr,nc,nb] = size(im2);
+    figure(2); imagesc(im2); axis image;
+    hold on; plot(x2,y2,'*'); hold off
+    for i=1:length(L)
+        a = L(1,i); b = L(2,i); c=L(3,i);
+        if (abs(a) > (abs(b)))
+           ylo=0; yhi=nr; 
+           xlo = (-b * ylo - c) / a;
+           xhi = (-b * yhi - c) / a;
+           hold on
+           h=plot([xlo; xhi],[ylo; yhi]);
+           set(h,'Color',colors(i),'LineWidth',2);
+           hold off
+           drawnow;
+        else
+           xlo=0; xhi=nc; 
+           ylo = (-a * xlo - c) / b;
+           yhi = (-a * xhi - c) / b;
+           hold on
+           h=plot([xlo; xhi],[ylo; yhi],'b');
+           set(h,'Color',colors(i),'LineWidth',2);
+           hold off
+           drawnow;
+        end
+    end
+    
+    
+    %overlay epipolar lines on im1
+    L = ([x2' ; y2'; ones(size(x2'))]' * F)' ;
+    [nr,nc,nb] = size(im);
+    figure(1); imagesc(im); axis image;
+    hold on; plot(x1,y1,'*'); hold off
+    for i=1:length(L)
+        a = L(1,i); b = L(2,i); c=L(3,i);
+        if (abs(a) > (abs(b)))
+           ylo=0; yhi=nr; 
+           xlo = (-b * ylo - c) / a;
+           xhi = (-b * yhi - c) / a;
+           hold on
+           h=plot([xlo; xhi],[ylo; yhi],'b');
+           set(h,'Color',colors(i),'LineWidth',2);
+           hold off
+           drawnow;
+        else
+           xlo=0; xhi=nc; 
+           ylo = (-a * xlo - c) / b;
+           yhi = (-a * xhi - c) / b;
+           hold on
+           h=plot([xlo; xhi],[ylo; yhi],'b');
+           set(h,'Color',colors(i),'LineWidth',2);
+           hold off
+           drawnow;
+        end
+    end
+    
+    
+    for j=1:3
+        for i=1:3
+            fprintf('%10g ',10000*F(j,i));
+        end
+        fprintf('\n');
+    end
+    
+    
+    F_with_hartley = F;
+
 end
 
 
 % Eight-point algorithm without Hartley preconditioning
-function [F, pts1, pts2] = eightpoint_without_hartley(im1, im2, pts1, pts2, fig1, fig2)
-    x1 = pts1(:, 1);
-    y1 = pts1(:, 2);
-    x2 = pts2(:, 1);
-    y2 = pts2(:, 2);
-    A = [];
+function [F_without_hartley] = eightpoint_without_hartley(im, im2)
+        
+    
+    figure(3); imagesc(im); axis image; drawnow;
+    figure(4); imagesc(im2); axis image; drawnow;
+    
+    
+    
+    figure(3); [x1,y1] = getpts;
+    figure(3); imagesc(im); axis image; hold on
     for i=1:length(x1)
-       A(i,:) = [x1(i)*x2(i) x1(i)*y2(i) x1(i) y1(i)*x2(i) y1(i)*y2(i) y1(i) x2(i) y2(i) 1];
+       h=plot(x1(i),y1(i),'*'); set(h,'Color','g','LineWidth',2);
+       text(x1(i),y1(i),sprintf('%d',i));
     end
+    hold off
+    drawnow;
+    
+    figure(4); imagesc(im2); axis image; drawnow;
+     [x2,y2] = getpts;
+    figure(4); imagesc(im2); axis image; hold on
+    for i=1:length(x2)
+       h=plot(x2(i),y2(i),'*'); set(h,'Color','g','LineWidth',2);
+       text(x2(i),y2(i),sprintf('%d',i));
+    end
+    hold off
+    drawnow;
+    
+    %without Hartley preconditioning
+    A = [];
+    for i = 1:length(x1)
+        A(i, :) = [x1(i) * x2(i), x1(i) * y2(i), x1(i), y1(i) * x2(i), y1(i) * y2(i), y1(i), x2(i), y2(i), 1];
+    end
+    
     %get eigenvector associated with smallest eigenvalue of A' * A
     [u,d] = eigs(A' * A,1,'SM');
     F = reshape(u,3,3);
+    
     %make F rank 2
     oldF = F;
     [U,D,V] = svd(F);
     D(3,3) = 0;
     F = U * D * V';
     
-    disp("Fundamental matrix F without Hartley preconditioning is: ");
-    disp(F);
     
-    sanity_check(im1, im2, F, pts1, pts2, fig1, fig2);
-end
-
-function sanity_check(im1, im2, F, pts1, pts2, fig1, fig2)
-    % Draw epipolar lines in Image 1 for points from Image 2
-    figure(fig1); imagesc(im1); axis image; hold on;
-    for i = 1:size(pts1, 1)
-        plot(pts1(i, 1), pts1(i, 2), 'og', 'MarkerSize', 8, 'LineWidth', 2); % Display points in green
-        text(pts1(i, 1), pts1(i, 2), sprintf('%d', i), 'Color', 'g', 'FontSize', 8); % Label points
-    end
-    hold off;
-
-    % Draw epipolar lines in Image 2 for points from Image 1
-    figure(fig2); imagesc(im2); axis image; hold on;
-    for i = 1:size(pts2, 1)
-        plot(pts2(i, 1), pts2(i, 2), 'og', 'MarkerSize', 8, 'LineWidth', 2); % Display points in green
-        text(pts2(i, 1), pts2(i, 2), sprintf('%d', i), 'Color', 'g', 'FontSize', 8); % Label points
-    end
-    hold off;
-    overlay_epipolar_lines(F, pts1, im2, fig2);
-    overlay_epipolar_lines(F', pts2, im1, fig1);
-end
-
-function overlay_epipolar_lines(F, pts, img, figureNum)
-    % generalized function to overlay epipolar lines given F, points, and figure
-    colors = 'bgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmyk';
-    L = F * [pts(:, 1)'; pts(:, 2)'; ones(1, size(pts, 1))]; %pts rows = number of points
-    [nr, nc, ~] = size(img);
-    figure(figureNum); imagesc(img); axis image;
-    hold on; plot(pts(:, 1), pts(:, 2), '*r'); 
-
-    
-    for i = 1:size(L, 2) %number of columns in L(=number of epipolar lines)
-        a = L(1, i); b = L(2, i); c = L(3, i);
-        if abs(a) > abs(b)
-            ylo=0; yhi = nr;
-            xlo = (-b * ylo - c) / a;
-            xhi = (-b * yhi - c) / a;
-            h=plot([xlo; xhi],[ylo; yhi]);
-            set(h,'Color',colors(i),'LineWidth',2);
-            
+    colors =  'bgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmyk';
+    %overlay epipolar lines on im2
+    L = F * [x1' ; y1'; ones(size(x1'))];
+    [nr,nc,nb] = size(im2);
+    figure(4); imagesc(im2); axis image;
+    hold on; plot(x2,y2,'*'); hold off
+    for i=1:length(L)
+        a = L(1,i); b = L(2,i); c=L(3,i);
+        if (abs(a) > (abs(b)))
+           ylo=0; yhi=nr; 
+           xlo = (-b * ylo - c) / a;
+           xhi = (-b * yhi - c) / a;
+           hold on
+           h=plot([xlo; xhi],[ylo; yhi]);
+           set(h,'Color',colors(i),'LineWidth',2);
+           hold off
+           drawnow;
         else
-            xlo = 0; xhi = nc;
-            ylo = (-a * xlo - c) / b;
-            yhi = (-a * xhi - c) / b;
-            h=plot([xlo; xhi],[ylo; yhi],'b');
-            set(h,'Color',colors(i),'LineWidth',2);
-
+           xlo=0; xhi=nc; 
+           ylo = (-a * xlo - c) / b;
+           yhi = (-a * xhi - c) / b;
+           hold on
+           h=plot([xlo; xhi],[ylo; yhi],'b');
+           set(h,'Color',colors(i),'LineWidth',2);
+           hold off
+           drawnow;
         end
     end
-    hold off;
+    
+    
+    %overlay epipolar lines on im1
+    L = ([x2' ; y2'; ones(size(x2'))]' * F)' ;
+    [nr,nc,nb] = size(im);
+    figure(3); imagesc(im); axis image;
+    hold on; plot(x1,y1,'*'); hold off
+    for i=1:length(L)
+        a = L(1,i); b = L(2,i); c=L(3,i);
+        if (abs(a) > (abs(b)))
+           ylo=0; yhi=nr; 
+           xlo = (-b * ylo - c) / a;
+           xhi = (-b * yhi - c) / a;
+           hold on
+           h=plot([xlo; xhi],[ylo; yhi],'b');
+           set(h,'Color',colors(i),'LineWidth',2);
+           hold off
+           drawnow;
+        else
+           xlo=0; xhi=nc; 
+           ylo = (-a * xlo - c) / b;
+           yhi = (-a * xhi - c) / b;
+           hold on
+           h=plot([xlo; xhi],[ylo; yhi],'b');
+           set(h,'Color',colors(i),'LineWidth',2);
+           hold off
+           drawnow;
+        end
+    end
+    
+    
+    for j=1:3
+        for i=1:3
+            fprintf('%10g ',10000*F(j,i));
+        end
+        fprintf('\n');
+    end
+
+    
+    
+    F_without_hartley = F;
 end
